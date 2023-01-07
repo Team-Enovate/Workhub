a474c3bd8f6a9c97aabca45d3f008ddacfe0bcbb
samailamalima <samuelmalima09@gmail.com>
2023-01-07 11:04:49 +0000

    Create pages and Routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { BrowserRouter as Router,  Routes, Route} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import ConnectWallet from "./pages/ConnectWallet";
+import Profile from "./pages/Profile";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <>
-      <h1>Work Hub</h1>
+     <Router>
+      <Routes>
+        <Route path = "/" element = {<Dashboard/>} />
+        <Route path = "/connectwallet" element = {<ConnectWallet/>} />
+        <Route path = "/profile" element = {<Profile/>} />
+        <Route path = "/projects" element = {<Projects/>} />
+      </Routes>
+     </Router>
     </>
   );
 }
